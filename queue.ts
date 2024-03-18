137ab2bb8530a281789ea5eba1108da906eeeea4
dennyzain <abbasdenny24@gmail.com>
2024-03-18 15:07:13 +0700

    feat: add comment time complexities

--- queue.ts
+++ queue.ts
@@ -1,12 +1,16 @@
 class Queue {
   constructor(private stack: number[] = []) {}
+  // ------ time complexity of enqueue is O(1)
   enqueue(value: number) {
     this.stack.push(value);
   }
+
+  // ------ time complexity of dequeue is O(1)
   dequeue() {
     return this.stack.shift();
   }
 
+  // ------ time complexity of peek is O(1)
   peek() {
     return this.stack[0];
   }
