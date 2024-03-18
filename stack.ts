3d1c8fdab973f4cdc72a9d1f2a3d6d6eb0722bcd
dennyzain <abbasdenny24@gmail.com>
2024-03-18 15:08:23 +0700

    feat: add comment time complexities

--- stack.ts
+++ stack.ts
@@ -1,11 +1,17 @@
 class Stack {
   constructor(private stack: number[] = []) {}
+
+  // ------ time complexity of push is O(1)
   push(value: number) {
     this.stack.push(value);
   }
+
+  // ------ time complexity of pop is O(1)
   pop() {
     return this.stack.pop();
   }
+
+  // ------ time complexity of peek is O(1)
   peek() {
     return this.stack[this.stack.length - 1];
   }
